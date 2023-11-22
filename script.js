f61c0c8c43f4f33e17b7ed7520c7a8fb8b840d14
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-22 12:20:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let element = document.createElement("div");
 element.id = "browser-info";
 
-element.innerHTML="You are using "+navigator.appName+" version "+navigator.appVersion;
+element.innerText="You are using "+navigator.appName+" version "+navigator.appVersion;
 document.body.appendChild(element);
 
 
