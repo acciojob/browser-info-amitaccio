4a468347b22acec7ab5aa25f24e1d1d98b1c569e
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-22 12:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let element = document.createElement("div");
+element.id = "browser-info";
+
+element.innerHTML="You are using "+navigator.appName+" version "+navigator.appVersion;
+document.body.appendChild(element);
+
+
+
+
+
